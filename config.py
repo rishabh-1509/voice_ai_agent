d2d1850ca9b1c5ca011e05fbb7a665c4e2dc0b4e
rishabh-1509 <rishabh09dwivedi@gmail.com>
2025-02-26 08:31:35 +0530

    --

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ from typing import List
 
 class Settings(BaseSettings):
     # VideoSDK Configuration
-    VIDEOSDK_API_KEY: str
-    VIDEOSDK_MEETING_ID: str
-    
+    VIDEOSDK_API_KEY = "70a16e9b-8d50-4f35-a82c-802cf2764fa6"
+    VIDEOSDK_MEETING_ID = "h6c3-lpwr-5a9q"
     # AI Configuration
     LLM_MODEL: str = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
     EMBEDDING_MODEL: str = "sentence-transformers/all-MiniLM-L6-v2"
